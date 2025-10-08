0bdb3eb2f41936db5a7b48478b3081ac213af595
Les Naufragés - Vintage Story <lesnaufrages.rp@gmail.com>
2025-10-08 13:59:02 +0000

    init 1.0

---
+++ OSL/Windows Bash Script/command.cmd
@@ -0,0 +1,26 @@
+:: Si à un endroit il est noté [Var] alors c'est une variable et devez supprimer le [Var] et y mettre une lettre ou autre. 
+:: 
+:: Initialiser l'endroit ou travaillera le script. Dans le cas donné en cours le 08/10,
+:: C'étais sur une clef USB, ou vous avez tout supprimer :pepe_laught:
+
+
+:: Remplacer [Letters] par la lettre de votre Stockages.
+[Letters]:
+
+:: rmdir = remove directory
+:: remplacer [Directory] par le dossier choisis. Attention à la casses.
+rmdir [Directory] 
+
+:: Si vous souhaitez souhaitez supprimer TOUT le contenu de l'emplacement :
+:: rmdir /S = supprime les dossiers et tout leurs contenus.
+:: Mais il faut indiquer l'endroit ou le faire.
+::
+:: Vous pouvez également ajouter /Q pour ne pas avoir de confirmation.
+:: Attention à bien indiquer l'endroit ou le faire et quoi. 
+:: Remplacer [Origin] par l'endroit choisis. Attention à la casses.
+:: * signifie tout le contenu.
+rmdir [Origin]\* /S 
+:: rmdir [Origin]\* /S /Q  
+:: /Q pour ne pas avoir de confirmation.
+
+pause
